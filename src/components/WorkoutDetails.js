f9ab5f7a1a3b28fdf63ffe5bf5548d70b4c1b2ec
suraj.be <suraj.be@media.net>
2023-01-27 17:58:02 +0530

    Feat: Formatted workout addition time

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,5 +1,6 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 import { MdOutlineDelete } from 'react-icons/md';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 function WorkoutDetails({ workout }) {
   const { dispatch } = useWorkoutsContext()
@@ -19,7 +20,7 @@ function WorkoutDetails({ workout }) {
     <h4>{workout.title}</h4>
     <p><strong>Load(kg): </strong>{workout.load}</p>
     <p><strong>Reps: </strong>{workout.reps}</p>
-    <p>{workout.createdAt}</p>
+    <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
     <span onClick={handleClick}><MdOutlineDelete /></span>
   </div>
 }
