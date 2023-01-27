860e5cce2fcb47062df9a2352807dc4b9b903717
suraj.be <suraj.be@media.net>
2023-01-27 17:29:48 +0530

    FeatL Validating input fields

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -6,7 +6,8 @@ function WorkoutForm() {
   const [load, setLoad] = useState('') /* let the default value be string and not number */
   const [reps, setReps] = useState('') /* let the default value be string and not number */
   const [error, setError] = useState(null)
-  const { workouts, dispatch } = useWorkoutsContext()
+  const [emptyFields, setEmptyFields] = useState([])
+  const { dispatch } = useWorkoutsContext()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -25,11 +26,13 @@ function WorkoutForm() {
 
     if (!response.ok) {
       setError(data.message)
+      setEmptyFields(data.emptyFields)
     }
     if (response.ok) {
       setTitle('')
       setLoad('')
       setReps('')
+      setEmptyFields([])
       setError(null)
       console.log('New Workout Added => ', data)
       dispatch({ type: 'CREATE_WORKOUT', payload: data })
@@ -42,17 +45,17 @@ function WorkoutForm() {
 
       <label>
         <span>Exercise Title:</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} className={emptyFields.includes('title') ? ' error ' : ''} />
       </label>
 
       <label>
         <span>Load(in kg):</span>
-        <input type="number" onChange={(e) => setLoad(e.target.value)} value={load} />
+        <input type="number" onChange={(e) => setLoad(e.target.value)} value={load} className={emptyFields.includes('load') ? ' error ' : ''} />
       </label>
 
       <label>
         <span>Reps:</span>
-        <input type="number" onChange={e => setReps(e.target.value)} value={reps} />
+        <input type="number" onChange={e => setReps(e.target.value)} value={reps} className={emptyFields.includes('reps') ? ' error ' : ''} />
       </label>
 
       <button>Add Workout</button>
